33a9cc50e488c40adda443023f0d783998c8447d
Yony Calsin <helloyonycalsin@gmail.com>
2021-09-17 08:45:42 -0500

    feat: 🎸 create ToolCard component

---
+++ src/components/ToolCard/ToolCard.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react'
+
+export interface ToolCardProps {
+  title: string
+  description: string
+}
+
+export function ToolCard(props: ToolCardProps) {
+  const { description, title } = props
+
+  return (
+    <div className="bg-gray-800 rounded-lg p-2 hover:bg-gray-700 cursor-pointer">
+      <h4 className="font-semibold text-xl">{title}</h4>
+      <p className="mt-1 text-gray-300">{description}</p>
+    </div>
+  )
+}
