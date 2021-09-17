4a5477385507275e4661e40356a75fae83687706
Yony Calsin <helloyonycalsin@gmail.com>
2021-09-17 08:45:58 -0500

    feat: 🎸 use ToolCard instead of plain html

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
-import React from 'react'
+import * as React from 'react'
+import { ToolCard } from './components/ToolCard/ToolCard'
 
 function App() {
   return (
-    <div>
-      <div className="py-8 sm:py-24 px-4 sm:px-6 lg:px-8 mx-auto max-w-screen-xl">
+    <div className="text-gray-100">
+      <div className="py-4 sm:py-12 px-2 sm:px-3 lg:px-4 mx-auto max-w-screen-xl">
         <div className="text-center">
-          <h2 className="text-base font-semibold tracking-wide uppercase">Tooly</h2>
-          <p className="text-xl text-gray-400">Tools.</p>
+          <h2 className="text-3xl font-semibold tracking-wide uppercase">Tooly</h2>
+        </div>
+
+        <div className="grid grid-cols-3 gap-2 mt-5">
+          <ToolCard
+            title="Slugger"
+            description="Smart, fast and easy to use online tool built to generate search engine friendly and user friendly URL
+              slugs."
+          />
+          <ToolCard
+            title="Base64 Decode & Encode"
+            description="Use our super handy online tool to encode or decode your data."
+          />
         </div>
       </div>
     </div>
