e166c803eef77df7b25ced79785f387b8e6b3e8c
Yony Calsin <helloyonycalsin@gmail.com>
2021-09-17 09:13:57 -0500

    style: 💄 disable the select

--- src/components/ToolCard/ToolCard.tsx
+++ src/components/ToolCard/ToolCard.tsx
@@ -9,7 +9,7 @@ export function ToolCard(props: ToolCardProps) {
   const { description, title } = props
 
   return (
-    <div className="bg-gray-800 rounded-lg p-2 hover:bg-gray-700 cursor-pointer">
+    <div className="bg-gray-800 rounded-lg p-2 hover:bg-gray-700 cursor-pointer select-none">
       <h4 className="font-semibold text-xl">{title}</h4>
       <p className="mt-1 text-gray-300">{description}</p>
     </div>
