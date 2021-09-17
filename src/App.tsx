9fff71fb79d6d57af65a5d909195301a263b6bb4
Yony Calsin <helloyonycalsin@gmail.com>
2021-09-17 08:25:58 -0500

    fix: 🐛 lint

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
     <div>
       <div className="py-8 sm:py-24 px-4 sm:px-6 lg:px-8 mx-auto max-w-screen-xl">
         <div className="text-center">
-          <h2 className="text-base font-semibold tracking-wide text-blue-600 uppercase">Tooly</h2>
+          <h2 className="text-base font-semibold tracking-wide uppercase">Tooly</h2>
           <p className="text-xl text-gray-400">Tools.</p>
         </div>
       </div>
