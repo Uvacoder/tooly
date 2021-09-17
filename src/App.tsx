3d9dc9e5ac8e0a7e047069b2433ff2478ababe9e
Yony Calsin <helloyonycalsin@gmail.com>
2021-09-17 08:23:28 -0500

    feat: 🎸 custom the tailwind init file

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function App() {
   return (
-    <div className="bg-white">
-      <div className="py-16 sm:py-24 px-4 sm:px-6 lg:px-8 mx-auto max-w-screen-xl">
+    <div>
+      <div className="py-8 sm:py-24 px-4 sm:px-6 lg:px-8 mx-auto max-w-screen-xl">
         <div className="text-center">
           <h2 className="text-base font-semibold tracking-wide text-blue-600 uppercase">Tooly</h2>
           <p className="text-xl text-gray-400">Tools.</p>
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import 'tailwindcss/tailwind.css'
 import App from './App'
+
+import './assets/css/tailwindcss.css'
 
 ReactDOM.render(
   <React.StrictMode>
