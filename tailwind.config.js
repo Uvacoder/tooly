622635ebc7dc90f4d1e01944764dff42d23fbd31
Yony Calsin <helloyonycalsin@gmail.com>
2021-09-17 08:23:02 -0500

    feat: 🎸 use @yonycalsin/tailwindcss-theme config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,26 @@
+const { theme } = require('@yonycalsin/tailwindcss-theme')
+
+/**
+ * @type {import('@types/tailwindcss/tailwind-config').TailwindConfig}
+ */
 module.exports = {
   mode: 'jit',
   purge: ['./src/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      ...theme.colors,
+    },
+    spacing: {
+      ...theme.spacing,
+    },
     extend: {},
   },
   variants: {
     extend: {},
   },
   plugins: [],
+  corePlugins: {
+    float: false,
+  },
 }
